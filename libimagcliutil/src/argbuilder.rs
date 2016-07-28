fbb91c69082526028f8c6da47518e2b0d76d5459
Kai Sickeler <k.sickeler@gmail.com>
2016-07-28 16:33:51 +0200

    initial structure

---
+++ libimagcliutil/src/argbuilder.rs
@@ -0,0 +1,85 @@
+use std::option::Option;
+
+struct ArgBuilder<'a> {
+    name: &'a str,
+    long: Option<&'a str>,
+    short: Option<&'a str>,
+    takes_value: Option<bool>,
+    required: Option<bool>,
+    helptext: Option<&'a str>,
+    value_name: Option<&'a str>,
+}
+
+impl<'a> ArgBuilder<'a> {
+    pub fn new(name: &'a str) -> ArgBuilder<'a> {
+        ArgBuilder{
+            name:           name,
+            long:           None,
+            short:          None,
+            takes_value:    None,
+            required:       None,
+            helptext:       None,
+            value_name:     None,
+        }
+    }
+
+    pub fn with_long(mut self, long: &'a str) -> ArgBuilder<'a> {
+        self.long = Some(long);
+        self
+    }
+
+    pub fn without_long(mut self) -> ArgBuilder<'a> {
+        self.long = None;
+        self
+    }
+
+    pub fn with_short(mut self, short: &'a str) -> ArgBuilder<'a> {
+        self.short = Some(short);
+        self
+    }
+
+    pub fn without_short(mut self) -> ArgBuilder<'a> {
+        self.short = None;
+        self
+    }
+
+    pub fn with_takes_value(mut self, takes_value: bool) -> ArgBuilder<'a> {
+        self.takes_value = Some(takes_value);
+        self
+    }
+
+    pub fn without_takes_value(mut self) -> ArgBuilder<'a> {
+        self.takes_value = None;
+        self
+    }
+
+    pub fn with_required(mut self, required: bool) -> ArgBuilder<'a> {
+        self.required = Some(required);
+        self
+    }
+
+    pub fn without_required(mut self) -> ArgBuilder<'a> {
+        self.required = None;
+        self
+    }
+
+    pub fn with_helptext(mut self, helptext: &'a str) -> ArgBuilder<'a> {
+        self.helptext = Some(helptext);
+        self
+    }
+
+    pub fn without_helptext(mut self) -> ArgBuilder<'a> {
+        self.helptext = None;
+        self
+    }
+
+    pub fn with_value_name(mut self, value_name: &'a str) -> ArgBuilder<'a> {
+        self.value_name = Some(value_name);
+        self
+    }
+
+    pub fn without_value_name(mut self) -> ArgBuilder<'a> {
+        self.value_name = None;
+        self
+    }
+}
--- libimagcliutil/src/lib.rs
+++ libimagcliutil/src/lib.rs
@@ -1,3 +1,19 @@
+#![deny(
+    non_camel_case_types,
+    non_snake_case,
+    path_statements,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_allocation,
+    unused_import_braces,
+    unused_imports,
+    unused_mut,
+    unused_qualifications,
+    while_true,
+)]
+
+pub mod argbuilder;
+
 #[cfg(test)]
 mod tests {
     #[test]
