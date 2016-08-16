8dd919a93b57991da7a29e1b18fa70236866e124
Kai Sickeler <k.sickeler@gmail.com>
2016-08-16 13:25:03 +0200

    Added ViewSubCommand

---
+++ libimagcliutil/src/subcommand/view.rs
@@ -0,0 +1,11 @@
+build_subcommand_builder!(
+    ViewSubCommandBuilder,
+    view,
+    "view",
+    |builder: SubCommandBuilder<'a>| builder
+        .without_author()
+        .with_version("0.1")
+        .with_about("View about")
+        .with_usage("usage text")
+);
+
