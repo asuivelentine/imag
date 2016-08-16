5f163da9a22d7c8cfaf09186d71b5b46d0f6c302
Kai Sickeler <k.sickeler@gmail.com>
2016-08-16 13:24:18 +0200

    Added DeleteSubCommand

---
+++ libimagcliutil/src/subcommand/delete.rs
@@ -0,0 +1,11 @@
+build_subcommand_builder!(
+    DeleteSubCommandBuilder,
+    delete,
+    "delete",
+    |builder: SubCommandBuilder<'a>| builder
+        .without_author()
+        .with_version("0.1")
+        .with_about("Delete about")
+        .with_usage("usage text")
+);
+
