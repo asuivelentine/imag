06cd9fbabb3e517396eca7ce9ca25441ed977759
Matthias Beyer <mail@beyermatthias.de>
2016-09-04 16:17:48 +0200

    Do not specify about and usage

--- libimagcliutil/src/subcommand/add.rs
+++ libimagcliutil/src/subcommand/add.rs
@@ -5,7 +5,5 @@ build_subcommand_builder!(
     |builder: SubCommandBuilder<'a>| builder
         .with_author("imag authors, imag@imag-pim.org")
         .with_version("0.2.0")
-        .with_about("Add about")
-        .with_usage("usage text")
 );
 
